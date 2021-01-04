e81a853ca83e88ec21e0c5341b70839a429d9ed0
Your Username <you@example.com>
2021-01-04 11:24:52 -0500

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,24 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books JOIN series ON books.series_id = series.id ORDER BY books.year ASC LIMIT 3;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto FROM characters ORDER BY characters.motto ASC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species ,COUNT(characters.species) FROM characters WHERE characters.species = 'human';"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+ 
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title, characters.species,  FROM series JOIN characters on characters.id = characters.serie_id WHERE characters.species = 'human' "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*) FROM character_books JOIN characters ON character_books.character_id = characters.id GROUP BY characters.name ORDER BY COUNT(*) DESC, characters.name;"
 end
