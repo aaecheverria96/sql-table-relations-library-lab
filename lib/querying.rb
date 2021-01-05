5715702ee982b24c859636cfd01e60f62fa4ceca
Your Username <you@example.com>
2021-01-04 23:17:26 -0500

    commit tree 1d28392b4c9d1b1550a23d7d6bf1e2f6bc41a2b5

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,11 +12,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
- 
+ "SELECT  authors.name, subgenres.name FROM authors JOIN series on series.author_id =  authors.id JOIN subgenres on subgenres.id = series.subgenre_id  "
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.species,  FROM series JOIN characters on characters.id = characters.serie_id WHERE characters.species = 'human' "
+  "SELECT series.title  FROM series JOIN books on books.series_id = series.id JOIN character_books on character_books.book_id = books.id JOIN characters on characters.id = character_books.character_id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
